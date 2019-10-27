4974c332a578174f4531f840a30e2844b265bbfd
abhishek singh <abhishekcode1@gmail.com>
2019-10-27 22:50:01 +0530

    publish

---
+++ precache-manifest.0007f748c8b55d16ae6327fa799031bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d88e017f95600104cd48f03a445203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66122c458c944346a370",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0999bc07f5715531d9f4",
+    "url": "/static/js/2.874c54e4.chunk.js"
+  },
+  {
+    "revision": "66122c458c944346a370",
+    "url": "/static/js/main.7c708da9.chunk.js"
+  },
+  {
+    "revision": "f3395e579070aefd4c26",
+    "url": "/static/js/runtime-main.a84affc8.js"
+  }
+]);
