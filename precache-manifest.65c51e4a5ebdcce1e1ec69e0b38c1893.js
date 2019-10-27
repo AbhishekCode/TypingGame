8c1bddf36637eb877f406e6dd8593dbf9ce693d8
abhishek singh <abhishekcode1@gmail.com>
2019-10-27 22:55:22 +0530

    Fixed url

---
+++ precache-manifest.65c51e4a5ebdcce1e1ec69e0b38c1893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54834cd331af98fe6228c75b9e8da6a",
+    "url": "/TypingGame/index.html"
+  },
+  {
+    "revision": "17974db138ba99d22316",
+    "url": "/TypingGame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9356d199a78d2dc1a90a",
+    "url": "/TypingGame/static/js/2.cedb54d5.chunk.js"
+  },
+  {
+    "revision": "17974db138ba99d22316",
+    "url": "/TypingGame/static/js/main.3cf38837.chunk.js"
+  },
+  {
+    "revision": "67a0c3a9a832ab2d54c1",
+    "url": "/TypingGame/static/js/runtime-main.3b11e00a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0007f748c8b55d16ae6327fa799031bb.js"
+  "/TypingGame/precache-manifest.65c51e4a5ebdcce1e1ec69e0b38c1893.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TypingGame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
